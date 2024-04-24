81b2835914bb4a7ebfbbb912008a493386956a30
SamuelVergaraMartinIESBelen <svermar1707@g.educaand.es>
2024-04-24 20:38:35 +0200

    Initial commit

---
+++ src/githubnetbeans/GitHubNetBeans.java
@@ -0,0 +1,17 @@
+package githubnetbeans;
+
+/**
+ * Este es un proyecto de prueba para la actividad de GitHub y NetBeans. :)
+ * @author Samuel Vergara Martín
+ * @since 24/04/2024
+ */
+public class GitHubNetBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Javier Bustamante vs Dark Themes");
+    }
+    
+}
